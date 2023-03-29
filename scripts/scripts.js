38d9d8293ec66c8c8fa3cba94c5b544208b72f1a
Laura Heritier <laura.heritier@gmail.com>
2023-03-29 16:33:41 -0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
          initCounter();
         },
         offset: '50%'
-      });    
+      });
+      
+      setInterval('updateTimer()', 1000 );
 });
 
 function initCounter () {
@@ -35,4 +37,27 @@ function initCounter () {
       
         }); 
       });
+}
+
+function updateTimer() {
+  future  = Date.parse("May 31, 2023 11:30:00");
+  now     = new Date();
+  diff    = future - now;
+
+  days  = Math.floor( diff / (1000*60*60*24) );
+  hours = Math.floor( diff / (1000*60*60) );
+  mins  = Math.floor( diff / (1000*60) );
+  secs  = Math.floor( diff / 1000 );
+
+  d = days;
+  h = hours - days  * 24;
+  m = mins  - hours * 60;
+  s = secs  - mins  * 60;
+
+  document.getElementById("timer")
+    .innerHTML =
+      '<div class="timer-count col-lg-3">' + d + '<span> días</span></div>' +
+      '<div class="timer-count col-lg-3">' + h + '<span> horas</span></div>' +
+      '<div class="timer-count col-lg-3">' + m + '<span> minutos</span></div>' +
+      '<div class="timer-count col-lg-3">' + s + '<span> segundos</span></div>' ;
 }
